653ace275c299cf68852210b521d292ecb441e4c
farhad40 <you@example.com>
2023-08-15 13:08:51 +0330

    fix bog whit attrs

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,53 +1,49 @@
 <script>
 export default {
-    name: 'TreeView',
-    props: {
-        node: {
-            type: Object,
-            required: true,
-        },
+  name: 'TreeView',
+  props: {
+    node: {
+      type: Object,
+      required: true,
     },
-    methods: {
-        addFolder() {
-            const label = prompt('Enter folder name');
-            if (!label) return;
+  },
+  methods: {
+    addFolder() {
+      const label = prompt('Enter folder name');
+      if (!label) return;
 
-            this.$emit('addFolder', this.node, label);
-        },
-        addFile() {
-            const label = prompt('Enter file name');
-            if (!label) return;
-
-            this.$emit('addFile', this.node, label);
-        },
-        deleteNode() {
-            this.$emit('deleteNode', this.node);
-        },
+      this.$emit('addFolder', this.node, label);
     },
+    addFile() {
+      const label = prompt('Enter file name');
+      if (!label) return;
+
+      this.$emit('addFile', this.node, label);
+    },
+    deleteNode() {
+      this.$emit('deleteNode', this.node);
+    },
+  },
 };
 </script>
 
 <template>
   <div class="node" v-bind="$attrs">
-      <div class="node__details">
-          <div class="node__label">
-              {{ node.label }}
-              <small v-if="!node.children">(file)</small>
-          </div>
-          <div class="node__actions">
-              <button v-if="node.children" @click="addFolder">Add Folder</button>
-              <button v-if="node.children" @click="addFile">Add File</button>
-              <button v-if="node.id !== 1" @click="deleteNode">Delete</button>
-          </div>
+    <div class="node__details">
+      <div class="node__label">
+        {{ node.label }}
+        <small v-if="!node.children">(file)</small>
       </div>
-      <div class="node__children">
-          <TreeView
-              v-for="subnode in node.children"
-              :key="subnode.id"
-              :node="subnode"
-          />
+      <div class="node__actions">
+        <button v-if="node.children" @click="addFolder">Add Folder</button>
+        <button v-if="node.children" @click="addFile">Add File</button>
+        <button v-if="node.id !== 1" @click="deleteNode">Delete</button>
       </div>
     </div>
+    <div class="node__children">
+      <TreeView v-bind="$attrs" v-for="subnode in node.children" :key="subnode.id" :node="subnode" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
